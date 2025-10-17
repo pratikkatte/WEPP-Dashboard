fb2667e0f7061a79e92dc7d447cf5b72df876fed
pratikkatte <pratikkatte7@gmail.com>
2025-10-17 14:55:01 -0700

    readme update

--- start.sh
+++ start.sh
@@ -6,19 +6,38 @@ ln -sfn "$(realpath ./results)" /srv/wepp/results
 
 if [ -f ./results/projects.json ]; then
   echo "[WEPP-Dashboard] Found projects.json, retrieving project information..."
-  TAXONIUM_FILE_PATH=$(python3 -c "
+  read TAXONIUM_FILE_PATH TAXONIUM_SIZE < <(python3 -c "
 import json
 with open('./results/projects.json') as f:
     data = json.load(f)
+max_size = 0
+for v in data.values():
+    try:
+        size = int(v.get('taxonium_size', 0))
+        if size > max_size:
+            max_size = size
+    except Exception:
+        continue
 k = '${PROJECT_NAME}'
 if k in data:
-    print(data[k].get('taxonium_file_path', ''))
+    print(
+        data[k].get('taxonium_file_path', ''),
+        max_size,
+        sep=' '
+    )
 else:
-    print('Project name not found.')
-" )
+    print('Project name not found.', '')
+")
   if [ -n "$TAXONIUM_FILE_PATH" ] && [ "$TAXONIUM_FILE_PATH" != "Project name not found." ]; then
     echo "[WEPP-Dashboard] Using project: '${PROJECT_NAME}'"
     echo "[WEPP-Dashboard] taxonium_file_path: ${TAXONIUM_FILE_PATH}"
+
+    if [[ "${TAXONIUM_FILE_PATH}" == *.gz ]]; then
+        TAXONIUM_SIZE=$(( TAXONIUM_SIZE * 10 ))
+    fi
+    TAXONIUM_SIZE=$(( TAXONIUM_SIZE + 2048 ))
+    echo "[WEPP-Dashboard] taxonium_size: ${TAXONIUM_SIZE} MB"
+
   else
     echo "[WEPP-Dashboard] Error: Project name '${PROJECT_NAME}' not found in projects.json"
     exit 1
@@ -29,7 +48,7 @@ else
 fi
 
 echo "[WEPP-Dashboard] Launching dashboard backend server..."
-node --expose-gc --max-old-space-size=${NODE_MEMORY_LIMIT:-4096} \
+node --expose-gc --max-old-space-size=${TAXONIUM_SIZE:-4096} \
   /app/taxonium_backend/server.js \
   --port 8080 --data_file "results/$PROJECT_NAME/$TAXONIUM_FILE_PATH" \
   --config_json /app/taxonium_backend/config_public.json &
